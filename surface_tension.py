9381640551cc23a5c4ccfa15e38d8d7a9f3c731a
Vikas Kurapati <vikaskurapati@iitb.ac.in>
2018-05-27 03:44:37 +0530

    Working equilibrium rod and oscillating rod changes in the surface tension file.

--- surface_tension.py
+++ surface_tension.py
@@ -45,7 +45,8 @@ class ColorGradientAdami(Equation):
         d_cy[d_idx] = 0.0
         d_cz[d_idx] = 0.0
 
-    def loop(self, d_idx, d_cx, d_cy, d_cz, d_V, s_V, d_color, s_color, DWIJ, s_idx):
+    def loop(self, d_idx, d_cx, d_cy, d_cz, d_V, s_V, d_color, s_color, DWIJ, 
+             s_idx):
         c_i = d_color[d_idx]/(d_V[d_idx]*d_V[d_idx])
         c_j = s_color[s_idx]/(s_V[s_idx]*s_V[s_idx])
         factor = d_V[d_idx]*(c_i + c_j)
@@ -61,7 +62,8 @@ class MomentumEquationViscosityAdami(Equation):
         d_av[d_idx] = 0.0
         d_aw[d_idx] = 0.0
 
-    def loop(self, d_idx, d_V, d_au, d_av, d_aw, s_V, d_p, s_p, DWIJ, s_idx, d_m, R2IJ, XIJ, EPS, VIJ, d_nu, s_nu):
+    def loop(self, d_idx, d_V, d_au, d_av, d_aw, s_V, d_p, s_p, DWIJ, s_idx, 
+             d_m, R2IJ, XIJ, EPS, VIJ, d_nu, s_nu):
         factor = 2.0*d_nu[d_idx]*s_nu[s_idx]/(d_nu[d_idx] + s_nu[s_idx])
         V_i = 1/(d_V[d_idx]*d_V[d_idx])
         V_j = 1/(s_V[s_idx]*s_V[s_idx])
@@ -86,17 +88,6 @@ class MomentumEquationPressureGradientAdami(Equation):
         d_au[d_idx] += -(p_i+p_j)*DWIJ[0]/d_m[d_idx]
         d_av[d_idx] += -(p_i+p_j)*DWIJ[1]/d_m[d_idx]
         d_aw[d_idx] += -(p_i+p_j)*DWIJ[2]/d_m[d_idx]
-
-
-class SummationDensity(Equation):
-
-    def initialize(self, d_idx, d_V, d_rho):
-        d_rho[d_idx] = 0.0
-        d_V[d_idx] = 0.0
-
-    def loop(self, d_idx, d_V, d_rho, d_m, WIJ, s_idx):
-        d_rho[d_idx] += d_m[d_idx]*WIJ
-        d_V[d_idx] += WIJ
 
 
 class MomentumEquationViscosityMorris(Equation):
@@ -162,77 +153,6 @@ class InterfaceCurvatureFromDensity(Equation):
                 d_kappa[d_idx] /= d_wij_sum[d_idx]
 
 
-class MorrisColorGradient(Equation):
-
-    def __init__(self, dest, sources, epsilon=1e-6):
-        self.epsilon2 = epsilon*epsilon
-        super(MorrisColorGradient, self).__init__(dest, sources)
-
-    def initialize(self, d_idx, d_cx, d_cy, d_cz, d_nx, d_ny, d_nz,
-                   d_ddelta, d_N):
-
-        # color gradient
-        d_cx[d_idx] = 0.0
-        d_cy[d_idx] = 0.0
-        d_cz[d_idx] = 0.0
-
-        # interface normals
-        d_nx[d_idx] = 0.0
-        d_ny[d_idx] = 0.0
-        d_nz[d_idx] = 0.0
-
-        # reliability indicator for normals and dirac delta
-        d_N[d_idx] = 0.0
-        d_ddelta[d_idx] = 0.0
-
-    def loop(self, d_idx, s_idx, d_scolor, s_scolor, d_cx, d_cy, d_cz,
-             s_m, s_rho, DWIJ):
-
-        # Eq. (17) in [JM00]
-        Cba = (s_scolor[s_idx] - d_scolor[d_idx]) * s_m[s_idx]/s_rho[s_idx]
-
-        # color gradient
-        d_cx[d_idx] += Cba * DWIJ[0]
-        d_cy[d_idx] += Cba * DWIJ[1]
-        d_cz[d_idx] += Cba * DWIJ[2]
-
-    def post_loop(self, d_idx, d_cx, d_cy, d_cz,
-                  d_nx, d_ny, d_nz, d_N, d_ddelta):
-
-        # this is to avoid the very small values of
-        # normal direction which give spurious results
-        # of derivatives which build on and give unstable
-        # results at the interfaces
-        # if d_cx[d_idx]*d_cx[d_idx] < self.epsilon2:
-        #     d_cx[d_idx] = 0.0
-        # if d_cy[d_idx]*d_cy[d_idx] < self.epsilon2:
-        #     d_cy[d_idx] = 0.0
-        # if d_cz[d_idx]*d_cz[d_idx] < self.epsilon2:
-        #     d_cz[d_idx] = 0.0
-
-        # absolute value of the color gradient
-        mod_gradc2 = d_cx[d_idx]*d_cx[d_idx] + \
-            d_cy[d_idx]*d_cy[d_idx] + \
-            d_cz[d_idx]*d_cz[d_idx]
-
-        # avoid sqrt computations on non-interface particles
-        # (particles for which the color gradient is zero) Eq. (19,
-        # 20) in [JM00]
-        if mod_gradc2 > self.epsilon2:
-            # this normal is reliable in the sense of [JM00]
-            d_N[d_idx] = 1.0
-
-            # compute the normals
-            mod_gradc = 1./sqrt(mod_gradc2)
-
-            d_nx[d_idx] = d_cx[d_idx] * mod_gradc
-            d_ny[d_idx] = d_cy[d_idx] * mod_gradc
-            d_nz[d_idx] = d_cz[d_idx] * mod_gradc
-
-            # discretized Dirac Delta function
-            d_ddelta[d_idx] = 1./mod_gradc
-
-
 class FindMaxddelta(Equation):
 
     def reduce(self, dst, t, dt):
@@ -286,19 +206,6 @@ class SummationDensitySourceMass(Equation):
 
     def post_loop(self, d_idx, d_V, d_rho, d_m):
         d_V[d_idx] = d_rho[d_idx]/d_m[d_idx]
-
-
-class StateEquation(Equation):
-
-    def __init__(self, dest, sources, p0, rho0, gamma=7):
-        self.gamma = gamma
-        self.p0 = p0
-        self.rho0 = rho0
-        super(StateEquation, self).__init__(dest, sources)
-
-    def initialize(self, d_idx, d_p, d_rho):
-        factor = (d_rho[d_idx]/self.rho0)**(self.gamma)
-        d_p[d_idx] = self.p0*(factor - 1) + self.p0
 
 
 class AdamiReproducingDivergence(Equation):
